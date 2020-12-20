7a7945e1b67d0b5bb4a85b1fa7bedfb32798b762
madjid123 <dexmadjid@gmail.com>
2020-12-20 18:37:40 +0100

    Implementing the navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap'
+
+
+function NavBar() {
+
+    return (
+
+        <Navbar bg="dark" expand="lg" variant="dark">
+            <Navbar.Brand href="#home"> DexChatt </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Username</Nav.Link>
+
+
+                </Nav>
+
+                <Button variant="login" className="">Login</Button>
+
+
+            </Navbar.Collapse>
+        </Navbar>
+
+    );
+}
+
+export default NavBar;
