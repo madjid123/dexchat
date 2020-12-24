c484a278c71e4e0182d95a18acb0bbc92080906a
unknown <dexmadjid@gmail.com>
2020-12-24 16:04:46 +0100

    styling the input message

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useState } from 'react'
 
 function Conversation() {
+    const [message, setMessage] = useState("")
+    const handleSubmit = (e) => {
+        setMessage(e.target.value)
 
+
+    }
     return (
         <>
-            <form className="msgForm">
-                <input type="text"></input>
+            <form className="msgForm" onSubmit={handleSubmit} action='localhost:5000/sendmsg' method='get'>
+                <input type="text" name='msg'></input>
+                <input type="submit" value="send"></input>
             </form>
         </>
     )
