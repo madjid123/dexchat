a38e447d5698179a3b4fd3cb9c17d597ba7eda08
unknown <dexmadjid@gmail.com>
2020-12-25 23:59:34 +0100

    fldjs' ; ;

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { useState } from 'react'
-
-function Conversation() {
+import axios from 'axios'
+function Conversation(props) {
     const [message, setMessage] = useState("")
-    const handleSubmit = (e) => {
+    const handleChange = (e) => {
         setMessage(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setMessage(e.target.value)
+        axios.get('https://192.168.1.109:5000/sendmsg', message).then((res) => { console.log("Message is sent", res.json()) }).catch((error) => { console.log(error) })
+
 
 
     }
     return (
         <>
-            <form className="msgForm" onSubmit={handleSubmit} action='localhost:5000/sendmsg' method='get'>
-                <input type="text" name='msg'></input>
+            <form className="msgForm" onSubmit={handleSubmit} >
+                <input type="text" name='msg' onChange={handleChange}></input>
                 <input type="submit" value="send"></input>
             </form>
+            <a> {message}</a>
         </>
     )
 }
