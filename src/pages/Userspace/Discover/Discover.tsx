e62c190d6394a452a47e6e955bbfb8608e9ba1f6
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-03 16:01:51 +0100

    Creating Discover Component

---
+++ src/pages/Userspace/Discover/Discover.tsx
@@ -0,0 +1,20 @@
+import Input from "../../../components/Input/Input"
+
+export const Discover =()=>{
+    return <div className= "my-container">
+              <div className="flex-row">
+                <Input
+                  placeholder="Search for new contact"
+                  style={{ width: "90%", fontSize: "12px" }}
+                  variant="light"
+                />
+                <div>
+
+                    <div>dexcv fsd</div>
+                    <div>dexcv fsd</div>
+                    <div>dexcv fsd</div>
+                    <div>dexcv fsd</div>
+                </div>
+              </div>
+        </div>
+}
