5db05f8114a9ef9dadf5c4912fa50beceb5abf67
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-04 01:53:21 +0100

    create handleChange function for input

--- src/pages/Userspace/Discover/Discover.tsx
+++ src/pages/Userspace/Discover/Discover.tsx
@@ -1,19 +1,26 @@
+import { useState } from "react"
 import Input from "../../../components/Input/Input"
-
+import {Link} from "react-router-dom"
 export const Discover =()=>{
+    const [pattern, setPattern] = useState("")
+    const pseudoUsers = ["dxfd", "fkdjf" ,"erwtq","twqrt","trw"]
+    const handleChange= (e: React.SyntheticEvent) =>{
+                    const target = e.target as HTMLInputElement
+                    setPattern(target.value )
+
+                  }
     return <div className= "my-container">
               <div className="flex-row">
                 <Input
                   placeholder="Search for new contact"
-                  style={{ width: "90%", fontSize: "12px" }}
+                //   style={{ width: "90%", fontSize: "12px" }}
                   variant="light"
+                  value={pattern}
+                  onChange={handleChange}
                 />
-                <div>
-
-                    <div>dexcv fsd</div>
-                    <div>dexcv fsd</div>
-                    <div>dexcv fsd</div>
-                    <div>dexcv fsd</div>
+                <div className=""
+                style={{display: "flex", flexDirection: "column"}}>
+                  {pseudoUsers.map(pu => (<Link to="#ew"  >{pu}</Link>) )}
                 </div>
               </div>
         </div>
