bf685c9505793cdc7697f98031e567c8a9a3746a
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-15 21:47:00 +0100

    adding Dropdown, logo, fixing responsivness issues

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Dropdown } from "react-bootstrap";
 import Button from "../Button/Button";
 import { useState } from "react";
 import { Redirect, RouteProps, withRouter } from "react-router";
@@ -6,31 +6,24 @@ import { logout, AuthSelector } from "../../features/user/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./Header.css";
-// import { useTheme, Theme, styled, Container, Grid } from "@mui/material";
+import "./DropMenu.css"
+import { PersonCircle, MenuApp } from "react-bootstrap-icons"
 import styled from "styled-components";
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
-
-const NavBar: React.FunctionComponent<RouteProps & any> = ({
-  history,
-}: any) => {
+import DexLogo from "../../public/dexplanet.png"
+const NavBar: React.FunctionComponent<RouteProps & any> = (props: any) => {
   const dispatch = useDispatch();
   const { currentUser, isAuth } = useSelector(AuthSelector);
 
   const Logout = () => {
     dispatch(logout());
     const newPath = "/login";
-    history.push(newPath);
+    props.history.push(newPath);
     // <Redirect to="/login"></Redirect>;
   };
   let buttons = <> </>;
 
-  if (isAuth) {
-    buttons = (
-      <Button className="mx-2 " onClick={() => Logout()}>
-        Logout
-      </Button>
-    );
-  } else {
+  if (!isAuth)
     buttons = (
       <div className="header-content">
         <Button className="" href="/login">
@@ -41,47 +34,74 @@ const NavBar: React.FunctionComponent<RouteProps & any> = ({
         </Button>
       </div>
     );
-  }
+
+
 
   return (
-    <div className="header-nav">
+    <div className="header-nav ">
       <Navbar
         collapseOnSelect
         bg="tranparent"
         variant="dark"
-        className="Navbar"
-        expand="md"
+        className="Navbar m-auto px-4 align-items-center d-flex justify-content-space-between"
+        expand="sm"
+
       >
-       
-        <Navbar.Collapse id="basic-navbar-nav" className="">
-          <Nav className="m-auto">
-            {currentUser !== undefined && (
-              <div >
-                <Nav.Link className="text-white" href="#home">
-                  {currentUser.username}
-                </Nav.Link>
-                <div className="show-responsive-elmnts">
-                  <Nav.Link
-                    className="text-white"
-                    onClick={() => {
-                      history.push("/wello");
-                    }}
-                  >
-                    Rooms
-                  </Nav.Link>
-                  <Link to="/wello">madjid</Link>
+
+        <div className="logo d-flex align-items-center gap-1">
+          {currentUser !== undefined &&
+            <NavbarToggle
+              aria-controls="basic-navbar-nav"
+              style={{ color: "white", margin: "0.25rem" }}
+              onClick={props.handleShow}
+            >
+
+              {/* <MenuApp /> */}
+            </NavbarToggle>
+          }
+          <img src={DexLogo} width="24" height="28" alt="dexlogo" />
+        </div>
+        <div className="menu d-flex ">
+          <Navbar.Collapse id="basic-navbar-nav" className="" />
+          {currentUser !== undefined && (
+            <Dropdown
+              id="nav-dropdown-dark-example"
+              // menuVariant="dex"
+              // title={()}
+              align={{ md: "end" }}
+              className="text-white text-center d-flex justify-content-center"
+              style={{ display: "flex", justifyContent: "center", color: "white" }}
+            >
+              <Dropdown.Toggle id="" variant="" className="d-flex align-items-center p-2 dropdown-toggle-dex" as={"div"}>
+                <div className="flex justify-content-center ">
+
+                  <PersonCircle />
+                  <a className="text-white text-center text-decoration-none mx-2 justify-content-between" >
+                    {currentUser.username}
+                  </a>
                 </div>
-              </div>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-        {buttons}
-         <NavbarToggle
-          aria-controls="basic-navbar-nav"
-          style={{ color: "white", margin: "0.75rem" }}
-        />
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu variant="dex" style={{ color: "white!important" }} align="end">
+                <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Settings
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="" onClick={Logout} className="text-danger">
+                  Logout
+                </NavDropdown.Item>
+              </Dropdown.Menu>
+
+            </Dropdown>
+          )
+          }
+          {buttons}
+
+        </div>
+
       </Navbar>
-    </div>
+    </div >
   );
 };
 export default withRouter(NavBar);
