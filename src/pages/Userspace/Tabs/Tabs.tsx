6fd1bac5fade0e204d9c5b78ab903a1f4efd5022
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-15 21:40:19 +0100

    adding offcanas

--- src/pages/Userspace/Tabs/Tabs.tsx
+++ src/pages/Userspace/Tabs/Tabs.tsx
@@ -1,15 +1,20 @@
-import { Tab, Tabs } from "react-bootstrap";
+import { useState } from "react";
+import { Offcanvas, Tab, Tabs } from "react-bootstrap";
 import { Discover } from "../Discover/Discover";
 import Rooms from "../Rooms/Rooms";
 import "./Tabs.css";
-const SideTabs = () => {
+const SideTabs = (props: any) => {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
   return (
     <div className="sidetabs">
+
       <Tabs
         defaultActiveKey="rooms"
         id="uncontrolled-tab-example"
-        className="m-3 tabs "
-        // variant="pills"
+        className="m-3 tabs"
+        variant="pills"
       >
         <Tab eventKey="rooms" title="Rooms" className="tab">
           <Rooms />
@@ -17,8 +22,12 @@ const SideTabs = () => {
         <Tab eventKey="discover" title="Discover" className="tab">
           <Discover></Discover>
         </Tab>
+        <Tab eventKey="requests" title="Requests" className="tab">
+        </Tab>
+
+
       </Tabs>
-    </div>
+    </div >
   );
 };
 export default SideTabs;
