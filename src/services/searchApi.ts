19fa459300b959f0155f471da774aed22d47f0e6
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-20 20:33:22 +0100

    completing all api end-points

--- src/services/searchApi.ts
+++ src/services/searchApi.ts
@@ -15,6 +15,14 @@ export const SearchEndPointAPI = createApi({
                 return rawResult
             }
         }),
+        getUserByPattern: builder.query<any, { pattern: string, user_id: string }>({
+            query: (args) => ({
+                url: `search/${args.user_id}/getuser/${args.pattern}`,
+            }),
+            transformResponse: (rawResult, meta) => {
+                return rawResult
+            }
+        }),
         joinRequest: builder.query<any, { user_id: string, other_user_id: string }>({
             query: (args) => ({
                 url: `join_room/${args.user_id}/request/${args.other_user_id}`
@@ -28,6 +36,11 @@ export const SearchEndPointAPI = createApi({
         joinAccept: builder.query<any, { user_id: string, other_user_id: string }>({
             query: (args) => ({
                 url: `join_room/${args.user_id}/accept/${args.other_user_id}`
+            })
+        }),
+        joinRemove: builder.query<any, { user_id: string, other_user_id: string }>({
+            query: (args) => ({
+                url: `join_room/${args.user_id}/remove/${args.other_user_id}`
             })
         }),
     })
