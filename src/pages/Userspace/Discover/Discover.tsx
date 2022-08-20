c72a536c9188769fa6914a992c2e04fcd053aceb
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-20 23:04:38 +0100

    merging search and get users hooks into one

--- src/pages/Userspace/Discover/Discover.tsx
+++ src/pages/Userspace/Discover/Discover.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react"
 import Input from "../../../components/Input/Input"
-import { Link } from "react-router-dom"
-import { useGetAllUsersQuery, useLazyGetAllUsersQuery } from "../../../services/searchApi"
+import { useLazyGetAllUsersQuery, useLazyGetUserByPatternQuery } from "../../../services/searchApi"
 import { useSelector } from "react-redux"
 import { AuthSelector } from "../../../features/user/authSlice"
 import { Nav } from "react-bootstrap"
@@ -9,19 +8,21 @@ import { Person, PersonPlusFill } from "react-bootstrap-icons"
 import Button from "../../../components/Button/Button"
 export const Discover = () => {
   const [pattern, setPattern] = useState("")
-  const handleChange = (e: React.SyntheticEvent) => {
-    const target = e.target as HTMLInputElement
-    setPattern(target.value)
-
-  }
+  const [triggerPatternQuery, dataPatternQuery, errorPatterQuery] = useLazyGetUserByPatternQuery()
   const { currentUser } = useSelector(AuthSelector)
   const [trigger, data, error,] = useLazyGetAllUsersQuery()
   useEffect(() => {
     if (currentUser !== undefined) {
-      trigger({ user_id: currentUser._id })
+      trigger({ pattern: pattern, user_id: currentUser._id })
     }
-  }, [currentUser])
-  console.log(data.isSuccess)
+  }, [currentUser, pattern])
+  const handleChange = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLInputElement
+    setPattern(target.value)
+    // if (currentUser !== undefined)
+    // trigger({ pattern: pattern, user_id: currentUser._id })
+
+  }
   return <div className="my-container">
     <div className="flex-row">
       <Input
@@ -33,6 +34,29 @@ export const Discover = () => {
       />
       <div className="p-3"
         style={{ display: "flex", flexDirection: "column" }}>
+        {/* {(pattern !== "") && dataPatternQuery.isSuccess && dataPatternQuery.data.users.map((user: any) => {
+          return <Nav.Item>
+            <div className="d-flex align-items-center justify-content-between p-2">
+              <div className="d-flex">
+                <div className="mx-1">
+                  <Person />
+                </div>
+                <div>
+                  {user.username}
+                </div>
+              </div>
+
+              <div className="text-warning mx-2">
+                <Button className="btn-warning">
+                  <PersonPlusFill className="text-success" />
+                </Button>
+              </div>
+
+            </div>
+
+          </Nav.Item>
+        })} */}
+
         {(data.isSuccess) && data.data.users.map((user: any) => {
           return <Nav.Item>
             <div className="d-flex align-items-center justify-content-between p-2">
