3173eebe5569c908f1741c2c53dbdea114aadeaa
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-22 21:38:27 +0100

    adding join request to button click

--- src/pages/Userspace/Discover/Discover.tsx
+++ src/pages/Userspace/Discover/Discover.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import Input from "../../../components/Input/Input"
-import { useLazyGetAllUsersQuery, User } from "../../../services/searchApi"
+import { useLazyGetAllUsersQuery, useLazyJoinRequestQuery, User } from "../../../services/searchApi"
 import { useSelector } from "react-redux"
 import { AuthSelector } from "../../../features/user/authSlice"
 import { Nav } from "react-bootstrap"
@@ -10,6 +10,8 @@ export const Discover = () => {
   const [pattern, setPattern] = useState("")
   const { currentUser } = useSelector(AuthSelector)
   const [trigger, data,] = useLazyGetAllUsersQuery()
+  const [triggerjoinRequest, dataJoinRequest,] = useLazyJoinRequestQuery()
+  const btnRef = useRef(null)
   useEffect(() => {
     if (currentUser !== undefined) {
       trigger({ pattern: pattern, user_id: currentUser._id, friend: "" })
@@ -20,9 +22,12 @@ export const Discover = () => {
     const target = e.target as HTMLInputElement
     setPattern(target.value)
   }
-  const handleRequestClick = (e: React.SyntheticEvent) => {
-
+  const handleRequestClick = (user_id: string) => {
+    console.log(user_id)
+    if (currentUser !== undefined)
+      triggerjoinRequest({ user_id: currentUser?._id, other_user_id: user_id })
   }
+  console.log(dataJoinRequest.error)
   return <div className="my-container">
     <div className="flex-row">
       <Input
@@ -32,11 +37,15 @@ export const Discover = () => {
         value={pattern}
         onChange={handleChange}
       />
+      <div>
+        {(dataJoinRequest.isError && <p className="text-danger">{(dataJoinRequest.error as any).data}</p>)}
+        {(dataJoinRequest.isSuccess && <p className="text-success">Request is sent!</p>)}
+      </div>
       <div className="p-3"
         style={{ display: "flex", flexDirection: "column" }}>
 
-        {(data.isSuccess) && data.data.map((user: User) => {
-          return <Nav.Item>
+        {(data.isSuccess) && data.data.map((user: User, index) => {
+          return <Nav.Item key={index}>
             <div className="d-flex align-items-center justify-content-between p-2">
               <div className="d-flex">
                 <div className="mx-1">
@@ -48,7 +57,8 @@ export const Discover = () => {
               </div>
 
               <div className="text-warning mx-2">
-                <Button className="btn-warning" onClick={handleRequestClick} >
+                {console.log(user.pendingRequest)}
+                <Button className={`btn-warning ${user.pendingRequest ? "disabled" : ""}`} onClick={() => handleRequestClick(user._id)} id={index} disable={user.pendingRequest} >
                   <PersonPlusFill className="text-success" />
                 </Button>
               </div>
