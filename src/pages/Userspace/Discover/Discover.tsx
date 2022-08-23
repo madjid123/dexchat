5ee4fd13a17bb2e331fa8897d717813d3b3c794f
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-23 15:48:25 +0100

    add Remove request hook

--- src/pages/Userspace/Discover/Discover.tsx
+++ src/pages/Userspace/Discover/Discover.tsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useRef, useState } from "react"
 import Input from "../../../components/Input/Input"
-import { useLazyGetAllUsersQuery, useLazyJoinRequestQuery, User } from "../../../services/searchApi"
+import {
+  SearchEndPointAPI,
+  useLazyGetAllUsersQuery,
+  useLazyJoinRequestQuery,
+  useLazyJoinRemoveQuery,
+  User
+} from "../../../services/searchApi"
+import { store } from "../../../app/store"
+
 import { useSelector } from "react-redux"
 import { AuthSelector } from "../../../features/user/authSlice"
 import { Nav } from "react-bootstrap"
-import { Person, PersonPlusFill } from "react-bootstrap-icons"
+import { Person, PersonDashFill, PersonPlusFill } from "react-bootstrap-icons"
 import Button from "../../../components/Button/Button"
 export const Discover = () => {
   const [pattern, setPattern] = useState("")
   const { currentUser } = useSelector(AuthSelector)
   const [trigger, data,] = useLazyGetAllUsersQuery()
   const [triggerjoinRequest, dataJoinRequest,] = useLazyJoinRequestQuery()
+  const [triggerRemoveRequest, dataRemoveRequest,] = useLazyJoinRemoveQuery()
   const btnRef = useRef(null)
   useEffect(() => {
     if (currentUser !== undefined) {
@@ -22,12 +31,36 @@ export const Discover = () => {
     const target = e.target as HTMLInputElement
     setPattern(target.value)
   }
-  const handleRequestClick = (user_id: string) => {
-    console.log(user_id)
-    if (currentUser !== undefined)
-      triggerjoinRequest({ user_id: currentUser?._id, other_user_id: user_id })
+  const handleRequestClick = (index: number) => {
+    let user_id: string
+    let user: User
+    let pendingRequest: boolean
+    if (data.data !== undefined) {
+      user = data?.data[index]
+      user_id = user._id
+
+    }
+    else
+      return
+    if (currentUser !== undefined) {
+      if (user.pendingRequest === false) {
+        triggerjoinRequest({ user_id: currentUser?._id, other_user_id: user_id })
+        pendingRequest = true
+      }
+      else {
+        triggerRemoveRequest({ user_id: currentUser?._id, other_user_id: user_id })
+        pendingRequest = false
+      }
+      store.dispatch(SearchEndPointAPI.util.updateQueryData("getAllUsers", {
+        user_id: currentUser._id, pattern: "", friend: ""
+      }, (draftUsers) => {
+        console.log("this is a draft")
+        draftUsers[index].pendingRequest = pendingRequest
+        return draftUsers
+      }))
+    }
+
   }
-  console.log(dataJoinRequest.error)
   return <div className="my-container">
     <div className="flex-row">
       <Input
@@ -38,7 +71,7 @@ export const Discover = () => {
         onChange={handleChange}
       />
       <div>
-        {(dataJoinRequest.isError && <p className="text-danger">{(dataJoinRequest.error as any).data}</p>)}
+        {(dataJoinRequest.isError && <p className="text-danger">{(dataJoinRequest.error as any).data.originalStatus !== 200}</p>)}
         {(dataJoinRequest.isSuccess && <p className="text-success">Request is sent!</p>)}
       </div>
       <div className="p-3"
@@ -57,9 +90,11 @@ export const Discover = () => {
               </div>
 
               <div className="text-warning mx-2">
-                {console.log(user.pendingRequest)}
-                <Button className={`btn-warning ${user.pendingRequest ? "disabled" : ""}`} onClick={() => handleRequestClick(user._id)} id={index} disable={user.pendingRequest} >
-                  <PersonPlusFill className="text-success" />
+                <Button className={` `} onClick={() => handleRequestClick(index)} id={index}
+                  variant={`${user.pendingRequest ? "danger" : ""}`} >
+                  {(!user.pendingRequest) ? <PersonPlusFill className="text" /> :
+                    <PersonDashFill className="text" onClick={() => console.log("person")} />
+                  }
                 </Button>
               </div>
 
