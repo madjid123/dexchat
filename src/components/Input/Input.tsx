872e505ae05054b1c3d19a2cac5ff702d0bb5dc8
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-28 17:26:26 +0100

    fix theming issues

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,6 @@
 import "./Input.css";
 import styled from "styled-components";
+const theme = { borderColor: "#30c982" }
 const Input = (props: any) => {
   return (
     <input className="footer-input " {...props}>
@@ -9,20 +10,27 @@ const Input = (props: any) => {
 };
 export default styled(Input)`
   && {
+    &:focus{
+    border-color: none;
+    /*outline: none;*/
+    width: 100%;
+    color: ${(props) => (props.variant === "light" ? "#222" : "#ccc")};
+    box-shadow:none;
+    border-width : 3px !important;
+    border-color : ${theme.borderColor} !important ;
+    border-radius : 1.5rem 0.75rem 1.5rem 0.75rem !important;
+    background-color: ${(props) =>
+    props.variant === "light" ? "#FFFFFFE7" : "#60606031"} !important;
+    }
+  }
+    color: ${(props) => (props.variant === "light" ? "#222" : "#fff")};
     margin: 0.25rem;
     border-radius: 1.5rem;
-    padding-block: 0.25rem;
-    /* background-color: rgba(0, 0, 0, 0.323); */
-    padding-inline: 0.5rem;
-    border-color: #0000;
-    &:focus{
- border-color: none;
-  outline: none;
-  width: 100%;
-    }
-    color: ${(props) => (props.variant === "light" ? "#222" : "#ddd")};
-  }
+    padding-block: 0.5rem;
+    border-width : 2px;
+    border-color : ${theme.borderColor};
+    padding-inline: 0.75rem;
     background-color: ${(props) =>
-    props.variant === "light" ? "#ffffff30" : "#00000031"};
+    props.variant === "light" ? "#FFFFFFE7" : "#00000031"} !important;
   }
 `;
