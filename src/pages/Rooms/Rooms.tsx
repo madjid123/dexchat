d1091ac9393a72d69d786396fb16c815e7898822
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-29 16:20:25 +0100

    moved to /components

---
+++ src/pages/Rooms/Rooms.tsx
@@ -0,0 +1,32 @@
+import Rooms from "../../components/UserSpace/Rooms/Rooms"
+import Header from "../../components/Header/Header"
+import { useSelector } from "react-redux"
+import { AuthSelector } from "../../features/user/authSlice"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+type RoomsPageProps = {
+
+}
+const RoomsPageRouter = (props: any) => {
+
+    const { currentUser, isAuth, isLoading } = useSelector(AuthSelector)
+    const navigate = useNavigate()
+    // useEffect(() => {
+    //     console.log(isAuth)
+    //     if (!isAuth) {
+    //         console.log(isAuth)
+    //         navigate("/login")
+    //         return
+    //     }
+    // }, [currentUser])
+    if (!isAuth) {
+        navigate("/login")
+    }
+    return (
+        <div className="my-container">
+            < Header  ></Header >
+            <Rooms isPage={true}></Rooms>
+        </div >
+    )
+}
+export default RoomsPageRouter  
