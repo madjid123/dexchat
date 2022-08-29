cb254d8d6eceb328bf4c1de9b9f87de94853270a
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-29 13:30:24 +0100

    useNavigate to redirect to /login when user !Auth

--- src/pages/RoomsPage/RoomsPage.tsx
+++ src/pages/RoomsPage/RoomsPage.tsx
@@ -1,25 +1,30 @@
 import Rooms from "../Userspace/Rooms/Rooms"
 import Header from "../../components/Header/Header"
 import { useSelector } from "react-redux"
-import { History } from "history"
 import { AuthSelector } from "../../features/user/authSlice"
-import { FC, useEffect } from "react"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 type RoomsPageProps = {
 
 }
 const RoomsPageRouter = (props: any) => {
 
-    const { currentUser, isAuth } = useSelector(AuthSelector)
-    useEffect(() => {
-        console.log(isAuth)
-        if (currentUser === undefined) {
-            props.history.push("/login")
-            return
-        }
-    }, [isAuth])
+    const { currentUser, isAuth, isLoading } = useSelector(AuthSelector)
+    const navigate = useNavigate()
+    // useEffect(() => {
+    //     console.log(isAuth)
+    //     if (!isAuth) {
+    //         console.log(isAuth)
+    //         navigate("/login")
+    //         return
+    //     }
+    // }, [currentUser])
+    if (!isAuth) {
+        navigate("/login")
+    }
     return (
         <div className="my-container">
-            < Header history={props.history} ></Header >
+            < Header  ></Header >
             <Rooms></Rooms>
         </div >
     )
