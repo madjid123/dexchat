18a748b961d785e96b739a188388d51b9d5307bb
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-29 13:29:41 +0100

    change extenstion to tsx

--- src/pages/Home/Home.jsx
+++
@@ -1,6 +0,0 @@
-import "./Home.css"
-import Header from "../../components/Header/Header"
-import { Container,Row,Col} from "react-bootstrap"
-export const Home = ()=>{
-    return <div className="my-container"> <Header></Header> </div>
-}
