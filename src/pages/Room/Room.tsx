d84a19254f8c67627573c50bedc23358bf6d7d59
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-30 23:01:20 +0100

    add roomId match CheckisAuth

--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -1,37 +1,44 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router"
+import { useNavigate, useMatch } from "react-router"
+import { Navigate } from "react-router-dom"
 import Header from "../../components/Header/Header"
 import Conversation from "../../components/UserSpace/Conversation/Conversation"
 import { clearAllMessages, setRoomId } from "../../features/Conversation/MessagesSlice"
-import { AuthSelector } from "../../features/user/authSlice"
+import { AuthSelector, CheckisAuth } from "../../features/user/authSlice"
+import { MessagesSelector } from "../../features/Conversation/MessagesSlice"
 type RoomProps = {
-
 }
 const Room: FC<RoomProps> = (props) => {
     const dispatch = useDispatch();
     const closeConversation = () => {
         dispatch(clearAllMessages({}));
-        dispatch(setRoomId(""));
     };
     const { currentUser, isAuth, isLoading } = useSelector(AuthSelector)
+    const { roomId } = useSelector(MessagesSelector)
+    const [show, setShow] = useState(false)
     const navigate = useNavigate()
-    // useEffect(() => {
-    //     console.log(isAuth)
-    //     if (!isAuth) {
-    //         console.log(isAuth)
-    //         navigate("/login")
-    //         return
-    //     }
-    // }, [currentUser])
-    if (!isAuth) {
-        navigate("/login")
-    }
+    const match = useMatch("/room/:id")
+    useEffect(() => {
+        if (roomId == "" && match?.params.id !== undefined)
+            dispatch(setRoomId(match?.params.id));
+    }, [roomId])
+    useEffect(() => {
+        dispatch(CheckisAuth())
+    })
+    const handleShow = () => setShow(true)
+    console.log(isAuth)
     return (
+        (!isAuth) && (<Navigate to="/login" />) ||
         <div className="my-container">
-            < Header  ></Header >
+            (console.log(isAuth))
+            < Header show={show} handleShow={handleShow}  ></Header >
             <Conversation closeConversation={closeConversation} isPage={true}></Conversation>
         </div >
     )
 }
 export default Room
+
+function dispatch(arg0: { payload: string; type: string }) {
+    throw new Error("Function not implemented.")
+}
