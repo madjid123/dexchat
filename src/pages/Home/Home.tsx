fb97acb38aa11bf567078d0ff4ba97d9351d3507
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-30 23:00:47 +0100

    add Header Props

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,9 +3,12 @@ import Header from "../../components/Header/Header"
 import { AuthSelector } from "../../features/user/authSlice"
 import { useSelector } from "react-redux"
 import { Navigate } from "react-router"
+import { useState } from "react"
 export const Home = () => {
     const { isAuth } = useSelector(AuthSelector)
+    const [show, setShow] = useState(false)
+    const handleShow = () => setShow(true)
     if (isAuth)
         return <Navigate to="/user" />
-    return <div className="my-container"> <Header></Header> </div>
+    return <div className="my-container"> <Header show={show} handleShow={handleShow}></Header> </div>
 }
