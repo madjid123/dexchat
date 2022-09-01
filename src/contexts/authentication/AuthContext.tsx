857a2aa17c10de97751231fd86ffdbca64dc2555
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-01 22:32:51 +0100

    rename context var to autState

--- src/contexts/authentication/AuthContext.tsx
+++ src/contexts/authentication/AuthContext.tsx
@@ -1,12 +1,12 @@
 import { createContext, useContext } from "react"
 import { AuthState, initialState } from "../../features/user/authSlice"
 export type AuthContext = {
-    initialState: AuthState
+    authState: AuthState
     setAuth: (value: AuthState) => void
 }
 
 
 export const MyAuthContext = createContext<AuthContext>({
-    initialState, setAuth: (value: AuthState) => { }
+    authState: initialState, setAuth: (value: AuthState) => { }
 } as AuthContext)
 export const useAuthContext = () => useContext(MyAuthContext)
