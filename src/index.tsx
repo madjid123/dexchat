5f3ee775da7f9e7ad7746fb90a4739a6d57fecbe
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-01 19:20:55 +0100

    Ascend BrowserRoute before store provider

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,12 @@ import { store } from "./app/store"
 import App from "./App"
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store} >
-      <BrowserRouter>
+    <BrowserRouter>
+      <Provider store={store} >
         <App />
-      </BrowserRouter>
 
-    </Provider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode >,
   document.getElementById('root')
 );
