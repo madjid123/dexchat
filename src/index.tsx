6b5d060d5078fef7abe518ee8ff22bef54ce74b4
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-01 20:02:22 +0100

    add PersistGate Provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,16 @@ import * as React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
-import { store } from "./app/store"
+import { store, persistor } from "./app/store"
+import { PersistGate } from "redux-persist/integration/react"
 import App from "./App"
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store} >
-        <App />
-
+        <PersistGate loading={null} persistor={persistor} >
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode >,
