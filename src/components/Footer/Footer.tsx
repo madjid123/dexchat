71455c9c28189c632dc60ceffd91baef59829992
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-02 21:05:06 +0100

    rename component to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,28 +1,22 @@
 import "./Footer.css";
-import { NavItem, Navbar, Nav, NavLink } from "react-bootstrap";
-import NavBar from "../Header/Header";
+import { NavItem, Navbar, Nav, NavLink, } from "react-bootstrap";
 export const FooterHeight = 10;
-export const SideBar = () => {
+export const Footer: React.FC = (_props) => {
   return (
-    <div className="my-footer">
-      <footer className="">
-        <Navbar
-          variant="dark"
-          fixed="bottom"
-          color="white"
-          className="my-footer"
-        >
-          <Navbar.Toggle />
-          <Navbar.Collapse>
-            <Nav variant="pills" justify={true} className="menu-items">
-              <NavLink>Home</NavLink>
-              <Nav.Link>Rooms</Nav.Link>
-              <Nav.Link>Settings</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </footer>
-    </div>
+    <Navbar
+      variant="dark"
+      color="white"
+      className="my-footer"
+    >
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav variant="pills" justify={true} className="menu-items">
+          <Nav.Link>Home</Nav.Link>
+          <Nav.Link>Rooms</Nav.Link>
+          <Nav.Link>Settings</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
-export default SideBar;
+export default Footer;
