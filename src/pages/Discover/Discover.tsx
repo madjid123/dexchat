b3cad83929d9698cdb63f6328668591a421d5815
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-06 20:51:08 +0100

    export DiscoverPage

--- src/pages/Discover/Discover.tsx
+++ src/pages/Discover/Discover.tsx
@@ -1,6 +1,6 @@
 import Header from "../../components/Header/Header"
 import { Discover } from "../../components/UserSpace/Discover/Discover"
-const DiscoverPage = () => {
+export const DiscoverPage = () => {
 
     return (<div className="box">
         <Header show={false} handleShow={() => { }} />
@@ -9,3 +9,4 @@ const DiscoverPage = () => {
 
     )
 }
+export default DiscoverPage
