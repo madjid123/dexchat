91db1ff8a4c6770fde4cc9e82439039cffe98068
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-07 22:28:29 +0100

    add styling

--- src/components/UserSpace/Tabs/Tabs.tsx
+++ src/components/UserSpace/Tabs/Tabs.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Col, Container, Nav, Offcanvas, Row, Tab, Tabs } from "react-bootstrap";
 import { Discover } from "../Discover/Discover";
 import { Requests } from "../Requests/Requests";
+import { Compass } from "react-bootstrap-icons"
 import Rooms from "../Rooms/Rooms";
 import "./Tabs.css";
 const SideTabs = (props: any) => {
@@ -29,28 +30,34 @@ const SideTabs = (props: any) => {
     //   </Tabs>
     // </div >
     <Tab.Container id="left-tabs-example" defaultActiveKey="first" >
-      <Container className="d-flex flex-column justify-content-between sidetabs  gap-2" >
-        <Row sm={10} md={12} className="box-container" >
+      <Container className="d-flex flex-column justify-content-between main-box gap-2 tabs" >
+        <div className="box-container" >
           <Tab.Content className="d-flex justify-content-center ">
-            <Tab.Pane eventKey="first" className="" >
+            <Tab.Pane eventKey="rooms" className="" >
               <Rooms isPage={false} />
             </Tab.Pane>
-            <Tab.Pane eventKey="second">
+            <Tab.Pane eventKey="discover">
               <Discover />
             </Tab.Pane>
+            <Tab.Pane eventKey="requests">
+              <Requests />
+            </Tab.Pane>
           </Tab.Content>
-        </Row>
-        <Row sm={10} md={13} >
+        </div>
+        <div>
           <Nav variant="pills" className="flex-row justify-content-center box-container">
-            <Nav.Item>
-              <Nav.Link eventKey="first">Tab 1</Nav.Link>
+            <Nav.Item className="">
+              <Nav.Link eventKey="rooms" className="tab">Rooms</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="second">Tab 2</Nav.Link>
+              <Nav.Link eventKey="discover"><Compass /></Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="requests">Requests</Nav.Link>
             </Nav.Item>
           </Nav>
 
-        </Row>
+        </div>
       </Container>
 
     </Tab.Container>
