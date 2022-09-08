536eaf42bef9bf8db6d84702bd141027850db9e1
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-08 19:46:43 +0100

    styling ...

--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -29,9 +29,11 @@ const Room: FC<RoomProps> = (props) => {
     const handleShow = () => setShow(true)
 
     return (
-        <div className="my-container sidebar">
+        <div className="main-box d-flex flex-column gap-0 mx-2 justify-content-around ">
             < Header show={show} handleShow={handleShow}  ></Header >
-            <Conversation closeConversation={closeConversation} isPage={true}></Conversation>
+            <div className="main-box box-container">
+                <Conversation closeConversation={closeConversation} isPage={true}></Conversation>
+            </div>
         </div >
     )
 }
