dc25ec31d9e0b3a1d00bb76efe4246c13950e828
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-11 12:48:53 +0100

    fix height issue of main-box

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -3,15 +3,13 @@ import SideTabs from "../UserSpace/Tabs/Tabs";
 
 export const Page: React.FC = (props) => {
     return (
-        <div className="d-flex flex-column justify-content-around mx-2 gap-1" style={{ height: "100vh" }}>
+        <div className="d-flex flex-column justify-content-evenly mx-2" style={{ height: "100vh" }}>
             <Header show={true} handleShow={() => { }}></Header>
-            <div className="main-box" style={{ flexGrow: 0.8 }}>
+            <div className="main-box box-container" style={{ height: "78%" }}>
                 {props.children}
             </div>
 
-            <div style={{ position: "sticky" }}>
-                <SideTabs />
-            </div>
+            <SideTabs />
 
         </div>
 
