c7f3e9b8273ad9561664de1cffa2851d2c6c6990
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-11 16:30:11 +0100

    add some necessary classes

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { MyTabsContext } from "../../contexts/TabsContext";
 import Header from "../Header/Header";
 import SideTabs from "../UserSpace/Tabs/Tabs";
 
@@ -5,10 +7,9 @@ export const Page: React.FC = (props) => {
     return (
         <div className="d-flex flex-column justify-content-evenly mx-2" style={{ height: "100vh" }}>
             <Header show={true} handleShow={() => { }}></Header>
-            <div className="main-box box-container" style={{ height: "78%" }}>
+            <div className="main-box box-container d-flex justify-content-center" style={{ height: "78%" }}>
                 {props.children}
             </div>
-
             <SideTabs />
 
         </div>
