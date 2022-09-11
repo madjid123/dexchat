6dd7c272aa9179e8b721547c5ba81d5eb6ccf6c1
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-11 16:31:11 +0100

    remove wrapped component

--- src/pages/Discover/Discover.tsx
+++ src/pages/Discover/Discover.tsx
@@ -2,11 +2,8 @@ import Header from "../../components/Header/Header"
 import { Discover } from "../../components/UserSpace/Discover/Discover"
 export const DiscoverPage = () => {
 
-    return (<div className="box">
-        <Header show={false} handleShow={() => { }} />
+    return (
         <Discover />
-    </div>
-
     )
 }
 export default DiscoverPage
