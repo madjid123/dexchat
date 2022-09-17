e24339ee4eef80ede9fd4e1a2818366702886381
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-17 23:30:56 +0100

    remove auth context

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react"
 import { Navigate, Outlet } from "react-router"
 import { CurrentUser } from "../../features/user/authSlice"
-import { useAuthContext } from "../../contexts/authentication/AuthContext"
+import { AuthSelector } from "../../features/user/authSlice"
+import { useSelector } from "react-redux"
 type PrivateRouteProps = {
 
 }
 
 const PrivateRoute = () => {
 
-    const { authState } = useAuthContext()
-    const { isAuth } = authState
+    const { isAuth } = useSelector(AuthSelector)
     const [user, setUser] = useState({} as CurrentUser | undefined)
     useEffect(() => {
 
