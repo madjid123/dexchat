d6020976b3ce95a008a9d8cd2c0938962642780f
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-17 23:30:46 +0100

    add propstype

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,10 +1,11 @@
+import { PropsWithChildren } from "react";
 import Header from "../Header/Header";
 import SideTabs from "../UserSpace/Tabs/Tabs";
 
-export const Page: React.FC = (props) => {
+export const Page: React.FC<PropsWithChildren> = (props) => {
     return (
         <div className="d-flex flex-column justify-content-evenly mx-2" style={{ height: "100vh" }}>
-            <Header show={true} handleShow={() => { }}></Header>
+            <Header show={false} handleShow={() => { }}></Header>
             <div className="main-box box-container d-flex justify-content-center" style={{ height: "78%" }}>
                 {props.children}
             </div>
