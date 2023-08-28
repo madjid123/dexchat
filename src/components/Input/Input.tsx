1ed98595a25811fb3c14062c484051436ec8973b
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-28 22:50:05 +0100

    change border style

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -12,19 +12,22 @@ export default styled(Input)`
   && {
     &:focus{
     border-color: none;
-    /*outline: none;*/
+    outline: none;
+    border-style : solid;
     width: 100%;
     color: ${(props) => (props.variant === "light" ? "#222" : "#ccc")};
     box-shadow:none;
     border-width : 3px !important;
+    
     border-color : ${theme.borderColor} !important ;
-    border-radius : 1.5rem 0.75rem 1.5rem 0.75rem !important;
+    border-radius : 1.5rem 0.5rem 1.5rem 0.5rem !important;
     background-color: ${(props) =>
     props.variant === "light" ? "#FFFFFFE7" : "#60606031"} !important;
     }
   }
     color: ${(props) => (props.variant === "light" ? "#222" : "#fff")};
     margin: 0.25rem;
+    border-style: solid;
     border-radius: 1.5rem;
     padding-block: 0.5rem;
     border-width : 2px;
