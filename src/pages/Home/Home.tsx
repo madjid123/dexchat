43ae0e3d4b72a2ea7f58e0b80b5f45bb5e441899
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-29 20:17:34 +0100

    fix responsivness issues

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -14,17 +14,17 @@ export const Home = () => {
         return <Navigate to="/rooms" />
     return <div className="my-container">
         <Header show={show} handleShow={handleShow}></Header>
-        <div className="home-container" style={{ color: "white" }}>
-            <div className="intro col-md-2">
-                <div className="intro-text">
-                    <h1>Your Ultimate Real-Time Chat website </h1>
+        <div className="home-container container-fluid " style={{ color: "white" }}>
+            <div className="intro m-0 px-4 px-md-1  flex flex-md-row flex-column justify-content-md-center align-items-md-center     ">
+                <div className="intro-text m-sm-0 gap-2   p-sm-0 ">
+                    <h1 className=" display-4 gradient-text">Your Ultimate Real-Time Chat website </h1>
                     <h3>Communicate, Share and interact</h3>
-                    <p>Welcome to ConnectChat, the cutting-edge solution that revolutionizes the way you communicate and collaborate online. With ConnectChat, real-time communication has never been easier, more interactive, and more efficient. Say goodbye to delayed responses and hello to instant, seamless conversations.DexChat provide a unique chat experience.  </p>
+                    <p>Welcome to DexChat, the cutting-edge solution that revolutionizes the way you communicate and collaborate online. With ConnectChat, real-time communication has never been easier, more interactive, and more efficient. Say goodbye to delayed responses and hello to instant, seamless conversations.DexChat provide a unique chat experience.  </p>
                     <div style={{ display: "flex", justifyContent: "center" }}>
                         <button onClick={() => { navigate("/register") }} className="landing-button"> Join Us Now!</button>
                     </div>
                 </div>
-                <img src="/landing/illust1.png" width={512} height={512} className=" w-auto " />
+                <img src="/landing/illust1.png" width={256} height={256} className="  d-flex  justify-content-center align-items-center ratio-1x1 img-fluid   " style={{ maxWidth: "70vw", minWidth: "50%" }} />
 
             </div>
         </div>
