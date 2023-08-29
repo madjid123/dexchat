538e697bca08994dffd455b294ce007991fccc3d
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-29 20:16:37 +0100

    fix flex issues

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -4,9 +4,9 @@ import SideTabs from "../UserSpace/Tabs/Tabs";
 
 export const Page: React.FC<PropsWithChildren> = (props) => {
     return (
-        <div className="d-flex flex-column justify-content-evenly mx-2" style={{ height: "100vh" }}>
+        <div className="d-flex flex-column justify-content-between p-2 gap-2 " style={{ height: "100vh" }}>
             <Header show={false} handleShow={() => { }}></Header>
-            <div className="main-box box-container d-flex justify-content-center" style={{ height: "78%" }}>
+            <div className="main-box box-container d-flex justify-content-center" style={{ height: "100%" }}>
                 {props.children}
             </div>
             <SideTabs />
