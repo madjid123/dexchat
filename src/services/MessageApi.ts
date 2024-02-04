b9d216cfbb58766fc1e59a2a327ec182422da485
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-04 23:20:58 +0100

    Refactor MessageApi.ts

--- src/services/MessageApi.ts
+++ src/services/MessageApi.ts
@@ -1,70 +1,63 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-import { Message, addMessage, MessagesState } from "../features/Conversation/MessagesSlice"
-import URL from "../URL"
-import { store } from "../app/store"
-import socket from "../utils/socket"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  Message,
+  addMessage,
+  MessagesState,
+} from "../features/Conversation/MessagesSlice";
+import URL from "../URL";
+import { store } from "../app/store";
+import socket from "../utils/socket";
 
 export const MessageEndPointApi = createApi({
-	reducerPath: "messagesApi",
-	baseQuery: fetchBaseQuery({ baseUrl: URL, credentials: "include", }),
-	tagTypes: ["Message"],
+  reducerPath: "messagesApi",
+  baseQuery: fetchBaseQuery({ baseUrl: URL, credentials: "include" }),
+  tagTypes: ["Message"],
 
-	endpoints: (builder) => ({
-		getMessagesByRoomId: builder.query<MessagesState, { room_id: string, page: number }>({
+  endpoints: (builder) => ({
+    getMessagesByRoomId: builder.query<
+      MessagesState,
+      { room_id: string; page: number }
+    >({
+      query: (args) => ({
+        url: `messages/room/${args.room_id}?page=${args.page}`,
+      }),
+      transformResponse: (rawResult, meta) => {
+        const result = rawResult as MessagesState;
 
-			query: (args) => ({
-				url: `messages/room/${args.room_id}?page=${args.page}`,
+        return result;
+      },
 
-
-			}),
-			transformResponse: (rawResult, meta) => {
-				console.log(rawResult);
-				const result = (rawResult as MessagesState)
-
-				return result
-
-			},
-
-			keepUnusedDataFor: 30,
-			onQueryStarted: async (arg, api) => {
-				try {
-
-					const { data } = await api.queryFulfilled
-
-
-				} catch (err) {
-					console.log(err)
-				}
-
-			},
-			onCacheEntryAdded: async (arg, api) => {
-				// create a websocket connection when the cache subscription starts
-				try {
-					socket.connect()
-					await api.cacheDataLoaded
-					const room = store.getState().MessagesReducer.room
-					const listener = (data: { message: Message, room: string }) => {
-						console.log("GETMSG", data)
-
-						api.updateCachedData((draft) => {
-							console.log(data)
-							if (room !== null && room._id !== data.room) return
-							draft.messagesResponse.messages.push(data.message)
-							store.dispatch(addMessage(data.message))
-							return draft
-						})
-					}
-					if (room != null) {
-						socket.on(`getmsg:${room._id}`, listener)
-					}
-
-				} catch (e) {
-					console.log((e as Error).message)
-				}
-				await api.cacheEntryRemoved
-			},
-
-		}),
-	})
-})
-export const { useGetMessagesByRoomIdQuery, useLazyGetMessagesByRoomIdQuery } = MessageEndPointApi
+      keepUnusedDataFor: 30,
+      onQueryStarted: async (arg, api) => {
+        try {
+        } catch (err) {
+          console.error(err);
+        }
+      },
+      onCacheEntryAdded: async (arg, api) => {
+        // create a websocket connection when the cache subscription starts
+        try {
+          socket.connect();
+          await api.cacheDataLoaded;
+          const room = store.getState().MessagesReducer.room;
+          const listener = (data: { message: Message; room: string }) => {
+            api.updateCachedData((draft) => {
+              if (room !== null && room._id !== data.room) return;
+              // draft.messagesResponse.messages.push(data.message)
+              store.dispatch(addMessage(data.message));
+              // return draft
+            });
+          };
+          if (room != null) {
+            socket.on(`getmsg:${room._id}`, listener);
+          }
+        } catch (e) {
+          console.error((e as Error).message);
+        }
+        await api.cacheEntryRemoved;
+      },
+    }),
+  }),
+});
+export const { useGetMessagesByRoomIdQuery, useLazyGetMessagesByRoomIdQuery } =
+  MessageEndPointApi;
