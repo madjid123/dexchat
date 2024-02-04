c91419996fb2f17ac6bad5a0edac8de73e1c8a6d
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-04 23:20:49 +0100

    Refactor RoomsPageRouter component

--- src/pages/Rooms/Rooms.tsx
+++ src/pages/Rooms/Rooms.tsx
@@ -1,35 +1,23 @@
-import Rooms from "../../components/UserSpace/Rooms/Rooms"
-import Header from "../../components/Header/Header"
-import { useSelector } from "react-redux"
-import { AuthSelector } from "../../features/user/authSlice"
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import "./Rooms.css"
-import Footer from "../../components/Footer/Footer"
-type RoomsPageProps = {
+import Rooms from "../../components/UserSpace/Rooms/Rooms";
+import { useSelector } from "react-redux";
+import { AuthSelector } from "../../features/user/authSlice";
+import { useNavigate } from "react-router-dom";
+import "./Rooms.css";
+type RoomsPageProps = {};
+const RoomsPageRouter = (props: RoomsPageProps) => {
+  const { isAuth } = useSelector(AuthSelector);
+  const navigate = useNavigate();
 
-}
-const RoomsPageRouter = (props: any) => {
-
-    const { currentUser, isAuth, isLoading } = useSelector(AuthSelector)
-    const navigate = useNavigate()
-    // useEffect(() => {
-    //     console.log(isAuth)
-    //     if (!isAuth) {
-    //         console.log(isAuth)
-    //         navigate("/login")
-    //         return
-    //     }
-    // }, [currentUser])
-    if (!isAuth) {
-        navigate("/login")
-    }
-    return (
-        <div className=" d-flex flex-column justify-center align-center" style={{ height: "100%" }}>
-
-            <Rooms isPage={true}></Rooms>
-
-        </div >
-    )
-}
-export default RoomsPageRouter  
+  if (!isAuth) {
+    navigate("/login");
+  }
+  return (
+    <div
+      className=" flex flex-col justify-center items-center"
+      style={{ height: "100%" }}
+    >
+      <Rooms isPage={true}></Rooms>
+    </div>
+  );
+};
+export default RoomsPageRouter;
