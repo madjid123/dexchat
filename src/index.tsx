2bce59d810e928df56e71cf6a789559d396d1f4d
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-04 23:16:48 +0100

    Add TabsProvider to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
-import * as React from "react"
-import ReactDOM from "react-dom"
-import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
-import { store, persistor } from "./app/store"
-import { PersistGate } from "redux-persist/integration/react"
-import App from "./App"
-ReactDOM.render(
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store, persistor } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import { TabsProvider } from "./providers/TabsProvider";
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("app");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store} >
-        <PersistGate loading={null} persistor={persistor} >
-          <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <TabsProvider>
+            <App />
+          </TabsProvider>
         </PersistGate>
       </Provider>
     </BrowserRouter>
-  </React.StrictMode >,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
