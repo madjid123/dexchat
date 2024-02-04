eff09a73121e45dd85f6e5d55a5da3ef2b5bebb5
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-04 23:18:46 +0100

    Refactor useThemeMode hook to use double quotes for string literals

--- src/hooks/useThemeMode.ts
+++ src/hooks/useThemeMode.ts
@@ -1,17 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export const useThemeMode = () => {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState("dark");
 
   const setMode = (mode: string) => {
-    window.localStorage.setItem('theme', mode);
+    window.localStorage.setItem("theme", mode);
     setTheme(mode);
   };
 
-  const themeToggler = () => (theme === 'dark' ? setMode('light') : setMode('dark'));
+  const themeToggler = () =>
+    theme === "dark" ? setMode("light") : setMode("dark");
 
   useEffect(() => {
-    const localTheme = window.localStorage.getItem('theme');
+    const localTheme = window.localStorage.getItem("theme");
     localTheme && setTheme(localTheme);
   }, []);
 
