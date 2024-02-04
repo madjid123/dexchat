f6add15bd1f50af01e16d6f6ea6bff4fb0082cf9
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-04 23:16:43 +0100

    Refactor React Router imports and update component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,101 @@
 // React
-import {
-  BrowserRouter as Router,
-  Route,
-  Navigate,
-  Routes,
-} from "react-router-dom";
-import { } from "react-router"
+import { Route, Navigate, Routes } from "react-router-dom";
+import {} from "react-router";
 import { Fragment, useEffect, useState } from "react";
 
 //Redux
-import { useSelector, useDispatch } from "react-redux";
-import { useAppDispatch } from "./app/hooks"
+import { useSelector } from "react-redux";
+import { useAppDispatch } from "./app/hooks";
 // Components
 import Register from "./pages/Auth/Register/Register";
 import Login from "./pages/Auth/Login/Login";
 import UserSpace from "./pages/Userspace/UserSpace";
 import Rooms from "./pages/Rooms/Rooms";
 // Styling
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { AuthSelector, CheckisAuth } from "./features/user/authSlice";
-import { darkTheme, lightTheme } from "./components/Theme/Theme";
-import { Home } from "./pages/Home/Home";
-import { ThemeProvider } from "styled-components";
-import Room from "./pages/Room/Room";
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import DiscoverPage from "./pages/Discover/Discover";
-import { Page } from "./components/Page/Page";
+import Appcss from "./App.css";
+import "./tailwind.css";
+import { AuthSelector, CheckisAuth } from "~/features/user/authSlice";
+import { Home } from "~/pages/Home/Home";
+import Room from "~/pages/Room/Room";
+import PrivateRoute from "~/components/PrivateRoute/PrivateRoute";
+import DiscoverPage from "~/pages/Discover/Discover";
+import { Layout } from "~/components/Layout/Layout";
 import { MyTabsContext } from "./contexts/TabsContext";
 import { RequestsPage } from "./pages/Requests/Requests";
 const App = () => {
   const dispatch = useAppDispatch();
   const AuthState = useSelector(AuthSelector);
-  const { currentUser, isAuth, isLoading } = AuthState;
-  const [currentEventKey, setCurrenEventKey] = useState("rooms")
+  const { isAuth } = AuthState;
   useEffect(() => {
     dispatch(CheckisAuth());
-  }, [isAuth]);
+  }, [isAuth, dispatch]);
   return (
     // <ThemeProvider theme={darkTheme}>
     <Fragment>
-      <MyTabsContext.Provider value={{ currentEventKey: currentEventKey, setEventKey: setCurrenEventKey }}>
-        <Routes>
-          <Route path="/user" element={<UserSpace />}>
-          </Route>
-          <Route path="/login" element={(!isAuth) ? <Login /> : <Navigate to="/" />}>
-          </Route>
-          <Route path="/register" element={<Register />}>
-          </Route>
-          <Route path="/rooms" element={<Page><Rooms /></Page>} />
-          <Route path="/room/"
-            element={<PrivateRoute />} >
-            <Route path=":id" element={<Page><Room /></Page>} />
-          </Route>
-          <Route path="/discover/"
-            element={<PrivateRoute />} >
-            <Route path="" element={<Page><DiscoverPage /></Page>} />
-          </Route>
-          <Route path="/requests/"
-            element={<PrivateRoute />} >
-            <Route path="" element={<Page><RequestsPage /></Page>} />
-          </Route>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </MyTabsContext.Provider >
-    </Fragment >
+      <Routes>
+        <Route
+          path="/user"
+          element={
+            <PrivateRoute>
+              <UserSpace />{" "}
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/login"
+          element={!isAuth ? <Login /> : <Navigate to="/" />}
+        ></Route>
+        <Route
+          path="/register"
+          element={!isAuth ? <Register /> : <Navigate to="/" />}
+        ></Route>
+        <Route
+          path="/rooms"
+          element={
+            <Layout>
+              <Rooms />
+            </Layout>
+          }
+        />
+        <Route path="/room/">
+          <Route
+            path=":id"
+            element={
+              <PrivateRoute>
+                <Layout>
+                  <Room />
+                </Layout>
+              </PrivateRoute>
+            }
+          />
+        </Route>
+        <Route path="/discover/">
+          <Route
+            path=""
+            element={
+              <PrivateRoute>
+                <Layout>
+                  <DiscoverPage />
+                </Layout>
+              </PrivateRoute>
+            }
+          />
+        </Route>
+        <Route path="/requests/">
+          <Route
+            path=""
+            element={
+              <Layout>
+                <RequestsPage />
+              </Layout>
+            }
+          />
+        </Route>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Fragment>
 
     // </ThemeProvider>
   );
