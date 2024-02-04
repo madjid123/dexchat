a6e18ca3fa72d7afe9cdbcc38afdf9977c48482a
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-04 23:18:06 +0100

    Refactor Discover component

--- src/components/UserSpace/Discover/Discover.tsx
+++ src/components/UserSpace/Discover/Discover.tsx
@@ -1,121 +1,97 @@
-import React, { useEffect, useRef, useState } from "react"
-import Input from "../../../components/Input/Input"
+import React, { useEffect, useState } from "react";
+import Input from "../../../components/Input/Input";
 import {
   SearchEndPointAPI,
   useLazyGetAllUsersQuery,
   useLazyJoinRequestQuery,
   useLazyJoinRemoveQuery,
-  User
-} from "../../../services/searchApi"
-import { store } from "../../../app/store"
-import "./Discover.css"
-import { useSelector } from "react-redux"
-import { AuthSelector } from "../../../features/user/authSlice"
-import { Nav } from "react-bootstrap"
-import { Person, PersonDashFill, PersonPlusFill } from "react-bootstrap-icons"
-import Button from "../../../components/Button/Button"
+  User,
+} from "../../../services/searchApi";
+import { store } from "../../../app/store";
+import "./Discover.css";
+import { useSelector } from "react-redux";
+import { AuthSelector } from "../../../features/user/authSlice";
+import { Nav } from "react-bootstrap";
+import { Person, PersonDashFill, PersonPlusFill } from "react-bootstrap-icons";
+import Button from "../../../components/Button/Button";
+import { useHandleRequest } from "~/hooks/UserSpace/Discover/useHandleRequest";
 export const Discover = () => {
-  const [pattern, setPattern] = useState("")
-  const { currentUser } = useSelector(AuthSelector)
-  const [trigger, data,] = useLazyGetAllUsersQuery()
-  const [triggerjoinRequest, dataJoinRequest,] = useLazyJoinRequestQuery()
-  const [triggerRemoveRequest, dataRemoveRequest,] = useLazyJoinRemoveQuery()
-  const btnRef = useRef(null)
-  useEffect(() => {
-    if (currentUser !== undefined) {
-      trigger({ pattern: pattern, user_id: currentUser._id, requests: "" })
-    }
-  }, [currentUser, pattern])
+  const [pattern, setPattern] = useState("");
+
   const handleChange = (e: React.SyntheticEvent) => {
-    e.preventDefault()
-    const target = e.target as HTMLInputElement
-    setPattern(target.value)
-  }
-  const handleRequestClick = (index: number) => {
-    var user_id: string
-    var user: User
-    var pendingRequest: boolean
-    if (data.data !== undefined) {
-      user = data?.data[index]
-      user_id = user._id
-
-    }
-    else
-      return
-    if (currentUser !== undefined) {
-      if (user.pendingRequest === false) {
-        triggerjoinRequest({ user_id: currentUser?._id, other_user_id: user_id })
-        pendingRequest = true
-      }
-      else {
-        triggerRemoveRequest({ user_id: currentUser?._id, other_user_id: user_id })
-        pendingRequest = false
-      }
-      store.dispatch(SearchEndPointAPI.util.updateQueryData("getAllUsers", {
-        user_id: currentUser._id, pattern: "", requests: ""
-      }, (draftUsers) => {
-        console.log("this is a draft")
-        draftUsers[index].pendingRequest = pendingRequest
-        return draftUsers
-      }))
-    }
-
-  }
+    e.preventDefault();
+    const target = e.target as HTMLInputElement;
+    setPattern(target.value);
+  };
+  const { discoverRooms, dataJoinRequest, handleRequestClick } =
+    useHandleRequest({
+      pattern,
+    });
   return (
-    <div className="my-container">
-      <h4>Discover new users</h4>
-      <div className="flex-row">
-        <Input
-          placeholder="Search for new contact"
-          style={{ width: "90%", fontSize: "12px", outline: "none" }}
-          variant="dark"
-          value={pattern}
-          onChange={handleChange}
-        />
-        <div>
-          {(dataJoinRequest.isError && <p className="text-danger">{(dataJoinRequest.error as any).data.originalStatus !== 200}</p>)}
-          {(dataJoinRequest.isSuccess && <p className="text-success">Request is sent!</p>)}
+    <div className="flex flex-col items-center gap-2">
+      <h3>Discover new users</h3>
+      <div className="flex flex-col  gap-2">
+        <div className="flex-row gap-4 flex">
+          <Input
+            placeholder="Search for new contact"
+            style={{ width: "90%", fontSize: "12px", outline: "none" }}
+            variant="dark"
+            value={pattern}
+            onChange={handleChange}
+          />
+          <div>
+            {dataJoinRequest.isError === true && (
+              <p className="text-danger">
+                {(dataJoinRequest.error as any).data.originalStatus !== 200}
+              </p>
+            )}
+            {dataJoinRequest.isSuccess && (
+              <p className="text-success">Request is sent!</p>
+            )}
+          </div>
         </div>
-        <div className="px-3"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            // overflow: "hidden",
-            // overflowY: "scroll",
-            height: "fit-content"
-          }}>
+        <div className="px- flex flex-col justify-start h-fit">
+          {discoverRooms.isSuccess &&
+            discoverRooms.data.map((user: User, index) => {
+              return (
+                <Nav.Item key={index} className="nav-item-dex">
+                  <div className="flex items-center justify-between  p-2">
+                    <div className="flex jusifiy-center items-center gap-1">
+                      <div className="">
+                        <Person />
+                      </div>
+                      <div>{user.username}</div>
+                    </div>
 
-          {(data.isSuccess) && data.data.map((user: User, index) => {
-            return <Nav.Item key={index} className="nav-item-dex" >
-              <div className="d-flex align-items-center justify-content-between  p-2">
-                <div className="d-flex">
-                  <div className="mx-1">
-                    <Person />
+                    <div className="!text-yellow-500 mx-2">
+                      {!user.to && (
+                        <Button
+                          className={` `}
+                          onClick={() => handleRequestClick(index)}
+                          variant={`${user.pendingRequest ? "danger" : ""}`}
+                        >
+                          {!user.pendingRequest ? (
+                            <PersonPlusFill className="text" />
+                          ) : (
+                            <PersonDashFill
+                              className="text"
+                              onClick={() => {}}
+                            />
+                          )}
+                        </Button>
+                      )}
+                      {user.to && (
+                        <p className="!text-yellow-500">
+                          has a request for You
+                        </p>
+                      )}
+                    </div>
                   </div>
-                  <div>
-                    {user.username}
-                  </div>
-                </div>
-
-                <div className="text-warning mx-2">
-                  {(!user.to) &&
-                    <Button className={` `} onClick={() => handleRequestClick(index)} id={index}
-                      variant={`${user.pendingRequest ? "danger" : ""}`} >
-                      {(!user.pendingRequest) ? <PersonPlusFill className="text" /> :
-                        <PersonDashFill className="text" onClick={() => console.log("person")} />
-                      }
-                    </Button>
-                  }{
-                    user.to && <p className="text-warning">has a request for You</p>
-                  }
-                </div>
-
-              </div>
-
-            </Nav.Item>
-          })}
+                </Nav.Item>
+              );
+            })}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
