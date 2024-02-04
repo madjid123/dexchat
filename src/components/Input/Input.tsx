af0abcae8efcac43b17873d793746780a847b2f8
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-04 23:17:29 +0100

    Refactor Input component styling

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,39 +1,39 @@
 import "./Input.css";
-import styled from "styled-components";
-const theme = { borderColor: "#30c982" }
-const Input = (props: any) => {
-  return (
-    <input className="footer-input " {...props}>
-      {props.children}
-    </input>
-  );
+import styled, { ThemedStyledProps } from "styled-components";
+const theme = { borderColor: "#30c982" };
+type InputPropsType = {
+  variant: "light" | "dark";
 };
-export default styled(Input)`
+type StyledInputProps = ThemedStyledProps<InputPropsType, any>;
+export default styled.input.attrs((props: StyledInputProps) => props)`
   && {
-    &:focus{
-    border-color: none;
-    outline: none;
-    border-style : solid;
-    width: 100%;
-    color: ${(props) => (props.variant === "light" ? "#222" : "#ccc")};
-    box-shadow:none;
-    border-width : 2px !important;
-    
-    border-color : ${theme.borderColor} !important ;
-    border-radius : 1.5rem 0.75rem 1.5rem 0.75rem !important;
-    background-color: ${(props) =>
-    props.variant === "light" ? "#FFFFFFE7" : "#60606031"} !important;
+    &:focus {
+      border-color: none;
+      outline: none;
+      border-style: solid;
+      width: 100%;
+      color: ${(props) => (props.variant === "light" ? "#222" : "#ccc")};
+      box-shadow: none;
+      border-width: 2px !important;
+
+      /* border-color: ${theme.borderColor} !important ; */
+      border-radius: 8px 0px 8px 0px !important;
+      background-color: ${(props) =>
+        props.variant === "light" ? "#FFFFFFE7" : "#60606031"} !important;
     }
+    color: white !important;
   }
-    color: ${(props) => (props.variant === "light" ? "#222" : "#fff")};
-    margin: 0.25rem;
-    border-style: solid;
-    border-radius: 1.5rem;
-    padding-block: 0.5rem;
-    border-width : 2px;
-    border-color : ${theme.borderColor};
-    padding-inline: 0.75rem;
-    background-color: ${(props) =>
+  width: 100%;
+  color: ${(props) =>
+    props.variant === "light" ? "#222 !important" : "#fff !important"};
+  margin: 0.25rem;
+  border-style: solid;
+  border-radius: 8px;
+  padding-block: 0.5rem;
+  border-width: 2px;
+  /* border-color: ${theme.borderColor}; */
+  /* @apply bg-secondary-500; */
+  padding-inline: 0.75rem;
+  background-color: ${(props) =>
     props.variant === "light" ? "#FFFFFFE7" : "#00000031"} !important;
-  }
 `;
