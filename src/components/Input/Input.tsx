306859fc38dc193ec2fd560ec2b7ffb69787749e
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-06 21:34:05 +0100

    Refactor Input component styling

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,39 +1,51 @@
+import { FC } from "react";
 import "./Input.css";
-import styled, { ThemedStyledProps } from "styled-components";
+import styled, {
+  StyledComponentProps,
+  ThemedStyledProps,
+} from "styled-components";
+import { cn } from "~/lib/utils";
 const theme = { borderColor: "#30c982" };
 type InputPropsType = {
   variant: "light" | "dark";
 };
 type StyledInputProps = ThemedStyledProps<InputPropsType, any>;
-export default styled.input.attrs((props: StyledInputProps) => props)`
+const StyledInput = styled.input.attrs((props: StyledInputProps) => props)`
   && {
     &:focus {
       border-color: none;
       outline: none;
       border-style: solid;
       width: 100%;
-      color: ${(props) => (props.variant === "light" ? "#222" : "#ccc")};
       box-shadow: none;
-      border-width: 2px !important;
-
-      /* border-color: ${theme.borderColor} !important ; */
+      border-width: 2px;
       border-radius: 8px 0px 8px 0px !important;
-      background-color: ${(props) =>
-        props.variant === "light" ? "#FFFFFFE7" : "#60606031"} !important;
     }
     color: white !important;
   }
   width: 100%;
-  color: ${(props) =>
-    props.variant === "light" ? "#222 !important" : "#fff !important"};
   margin: 0.25rem;
   border-style: solid;
   border-radius: 8px;
   padding-block: 0.5rem;
   border-width: 2px;
-  /* border-color: ${theme.borderColor}; */
-  /* @apply bg-secondary-500; */
   padding-inline: 0.75rem;
-  background-color: ${(props) =>
-    props.variant === "light" ? "#FFFFFFE7" : "#00000031"} !important;
 `;
+type InputProps = StyledComponentProps<"input", any, InputPropsType, never>;
+const Input: FC<InputProps> = (props) => {
+  return (
+    <StyledInput
+      {...props}
+      className={cn(
+        "border-2 focus:border-secondary-500 border-neutral-700 w-4/5 bg-black/10 ",
+
+        props.className
+      )}
+
+      // variant={props.variant}
+      // placeholder={props.placeholder}
+      // onChange={props.onChange}
+    />
+  );
+};
+export default Input;
