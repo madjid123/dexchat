6259dcd0a460634407fd1d9bcda1cf9429df7e00
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-08 15:48:20 +0100

    Update Header component with new logo and user menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 import { logout, AuthSelector } from "../../features/user/authSlice";
 import { useSelector } from "react-redux";
 import "./Header.css";
-
+import API_URL from "~/URL"
 import "./DropMenu.css";
 import {
   ArrowBarDown,
@@ -21,7 +21,7 @@ import {
 } from "../ui/dropdown-menu";
 import DexLogo from "../../public/dexplanet.png";
 import { useAppDispatch } from "../../app/hooks";
-import { Menu } from "lucide-react";
+import { Menu, UserCircle } from "lucide-react";
 import { useTabsContext } from "~/contexts/TabsContext";
 import { useSetCurrentMember } from "~/hooks/UserSpace/Conversation/useSetCurrentMemberName";
 import { setAllRooms } from "~/features/user/RoomsSlice";
@@ -53,8 +53,8 @@ const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
 
   if (!isAuth)
     buttons = (
-      <div className="flex flex-wrap justify-center items-center text-xs md:text-base gap-2">
-        <Button className=" ">
+      <div className="flex flex-wrap items-center justify-center gap-2 text-xs md:text-base ">
+        <Button className="">
           <a href="/login">Login</a>
         </Button>
         <Button className="">
@@ -64,63 +64,57 @@ const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
     );
 
   return (
-    <div
-      className=" sticky top-0  w-full p-4 rounded-xl  items-center  flex justify-between  shadow-[0_2px_10px_0px] shadow-primary-500/50 active:shadow-primary-500/50      backdrop-blur-md z-[1000] "
-      onFocus={(e) => {
-        // e.preventDefault();
-        console.log("header received focus event");
-        e.stopPropagation();
-      }}
-      onBlur={(e) => {
-        console.log("header received blur event");
-        e.stopPropagation();
-      }}
-    >
-      <div className="logo flex items-center gap-3 ">
-        {isAuth && (
-          <div className="md:hidden">
-            <Menu
-              className="fill-white text-white hover:text-slate-500"
-              onClick={() => setShowSidebar(!showSidebar)}
-            />
-          </div>
-        )}
-        <img
-          src={DexLogo}
-          width="24"
-          height="28"
-          alt="dexlogo"
-          onClick={() => {
-            navigate("/");
-          }}
-        />
-        <span className="text-white">DexChat</span>
-      </div>
-      <div className="menu flex ">
-        {currentUser !== undefined && (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild className="">
-              <div className="flex justify-center items-center hover:bg-white/30 p-2 rounded-xl ">
-                <PersonCircle className="fill-white" />
-                <span className="text-white text-center no-underline  mx-2 justify-between">
-                  {currentUser.username}
-                </span>
-                <ArrowDown className="fill-white font-bold" />
-              </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className=" backdrop-blur-md z-[1001] text-zinc-300 border-0 rounded-xl">
-              <DropdownMenuLabel>Account</DropdownMenuLabel>
-              <DropdownMenuItem>
-                <a href="#Profile">Profile</a>
-              </DropdownMenuItem>
-              <DropdownMenuItem>Setting</DropdownMenuItem>
-              <DropdownMenuItem onClick={Logout} className="text-red-400">
-                Logout
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-        {buttons}
+    <div className="w-full p-2">
+      <div
+        className=" sticky top-0  w-full p-4 rounded-xl  items-center  flex justify-between  shadow-[0_0px_10px_0px] shadow-primary-500/50 hover:shadow-primary-500/90 active:shadow-primary-500/50 hover:shadow-primary-500/90     backdrop-blur-md z-[1000] "
+
+      >
+        <div className="flex items-center gap-3 logo ">
+          {isAuth && (
+            <div className="md:hidden">
+              <Menu
+                className="text-white fill-white hover:text-slate-500"
+                onClick={() => setShowSidebar(!showSidebar)}
+              />
+            </div>
+          )}
+          <img
+            src={DexLogo}
+            width="24"
+            height="28"
+            alt="dexlogo"
+            onClick={() => {
+              navigate("/");
+            }}
+          />
+          <span className="text-white">DexChat</span>
+        </div>
+        <div className="flex menu ">
+          {currentUser !== undefined && (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild className="">
+                <div className="flex items-center justify-center p-2 hover:bg-white/30 rounded-xl ">
+                  {currentUser.image ? (<img src={`${API_URL}/${currentUser.image}`} className="w-6 h-6 border rounded-sm border-primary-500" /> || <UserCircle />) : <PersonCircle className="fill-white" />}
+                  <span className="justify-between mx-2 text-center text-white no-underline">
+                    {currentUser.username}
+                  </span>
+                  <ArrowDown className="font-bold fill-white" />
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className=" backdrop-blur-md z-[1001] text-zinc-300 border-0 rounded-xl">
+                <DropdownMenuLabel>Account</DropdownMenuLabel>
+                <DropdownMenuItem>
+                  <a href="#Profile">Profile</a>
+                </DropdownMenuItem>
+                <DropdownMenuItem>Setting</DropdownMenuItem>
+                <DropdownMenuItem onClick={Logout} className="text-red-400">
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+          {buttons}
+        </div>
       </div>
     </div>
   );
