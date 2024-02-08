9b23e9121abff34d1c1269009d6573acbdb610ce
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-08 15:48:51 +0100

    Delete DiscoverPage component

--- src/pages/Discover/Discover.tsx
+++
@@ -1,8 +0,0 @@
-import { Discover } from "../../components/UserSpace/Discover/Discover"
-export const DiscoverPage = () => {
-
-    return (
-        <Discover />
-    )
-}
-export default DiscoverPage
