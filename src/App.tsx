ec631a6db0ec02a828337b5cb8a8e9cc34722062
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-08 15:48:07 +0100

    Remove unused imports and routes

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import { useAppDispatch } from "./app/hooks";
 import Register from "./pages/Auth/Register/Register";
 import Login from "./pages/Auth/Login/Login";
 import UserSpace from "./pages/Userspace/UserSpace";
-import Rooms from "./pages/Rooms/Rooms";
 // Styling
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -18,12 +17,9 @@ import Appcss from "./App.css";
 import "./tailwind.css";
 import { AuthSelector, CheckisAuth } from "~/features/user/authSlice";
 import { Home } from "~/pages/Home/Home";
-import Room from "~/pages/Room/Room";
 import PrivateRoute from "~/components/PrivateRoute/PrivateRoute";
-import DiscoverPage from "~/pages/Discover/Discover";
 import { Layout } from "~/components/Layout/Layout";
 import { MyTabsContext } from "./contexts/TabsContext";
-import { RequestsPage } from "./pages/Requests/Requests";
 const App = () => {
   const dispatch = useAppDispatch();
   const AuthState = useSelector(AuthSelector);
@@ -51,48 +47,6 @@ const App = () => {
           path="/register"
           element={!isAuth ? <Register /> : <Navigate to="/" />}
         ></Route>
-        <Route
-          path="/rooms"
-          element={
-            <Layout>
-              <Rooms />
-            </Layout>
-          }
-        />
-        <Route path="/room/">
-          <Route
-            path=":id"
-            element={
-              <PrivateRoute>
-                <Layout>
-                  <Room />
-                </Layout>
-              </PrivateRoute>
-            }
-          />
-        </Route>
-        <Route path="/discover/">
-          <Route
-            path=""
-            element={
-              <PrivateRoute>
-                <Layout>
-                  <DiscoverPage />
-                </Layout>
-              </PrivateRoute>
-            }
-          />
-        </Route>
-        <Route path="/requests/">
-          <Route
-            path=""
-            element={
-              <Layout>
-                <RequestsPage />
-              </Layout>
-            }
-          />
-        </Route>
         <Route path="/" element={<Home />} />
       </Routes>
     </Fragment>
