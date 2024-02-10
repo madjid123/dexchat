19ec626478d41c75ed30d3433d4d77e53449c64d
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-10 14:21:43 +0100

    Update socket connection URL

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client"
 import API_URL from "~/URL"
-const socket = io(API_URL, {
+const socket = io(API_URL + ":5001", {
 	transports: ['websocket'],
 	upgrade: false
 })
