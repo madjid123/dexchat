20f8a38e6dcc23d49c3799b13858ed98b9189df6
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-11 16:29:28 +0100

    Update socket import and formatting

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,8 +1,8 @@
-import io from "socket.io-client"
-import API_URL from "~/URL"
+import io from 'socket.io-client';
+import API_URL from '~/URL';
 const socket = io(API_URL, {
-	transports: ['websocket'],
-	upgrade: false
-})
-socket.connect()
-export default socket
+  transports: ['websocket'],
+  upgrade: false,
+});
+socket.connect();
+export default socket;
