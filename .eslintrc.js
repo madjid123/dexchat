bd43a65170fbe3fbed50e62971272b844290a800
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-11 16:27:01 +0100

    Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,80 @@
 module.exports = {
-    extends: [
-      // By extending from a plugin config, we can get recommended rules without having to add them manually.
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:import/recommended',
-      'plugin:jsx-a11y/recommended',
-      'plugin:@typescript-eslint/recommended',
-      // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
-      // Make sure it's always the last config, so it gets the chance to override other configs.
-      'eslint-config-prettier',
-    ],
-    settings: {
-      react: {
-        // Tells eslint-plugin-react to automatically detect the version of React to use.
-        version: 'detect',
-      },
-      // Tells eslint how to resolve imports
-      'import/resolver': {
-        node: {
-          paths: ['src'],
-          extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        },
-      },
+
+
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended',
+
+    'plugin:@typescript-eslint/recommended',
+],
+  //   'plugin:@typescript-eslint/recommended',
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
+  ignorePatterns: ['vite.config.ts'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    typescript: true,project: ['./tsconfig.json'],
+    ecmaFeatures: {
+      jsx: true,
     },
-    rules: {
-      // Add your own rules here to override ones from the extended configs.
-    },
-  };
-  
+  },
+  rules: {
+    // 'prettier/prettier': 'error',
+  },
+
+
+  // parser: '@typescript-eslint/parser',
+  // plugins: ["@typescript-eslint"],
+  // extends: [
+  //   // By extending from a plugin config, we can get recommended rules without having to add them manually.
+  //   'eslint:recommended',
+  //   'plugin:react/recommended',
+  //   'plugin:import/recommended',
+  //   'plugin:jsx-a11y/recommended',
+  //   'plugin:@typescript-eslint/recommended',
+  //   'plugin:import/typescript',
+  //   "plugin:@typescript-eslint/recommended-requiring-type-checking",
+
+  //   // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
+  //   // Make sure it's always the last config, so it gets the chance to override other configs.
+  //   'eslint-config-prettier',
+  // ],
+  // settings: {
+  //   react: {
+  //     // Tells eslint-plugin-react to automatically detect the version of React to use.
+  //     version: 'detect',
+  //   },
+  //   // Tells eslint how to resolve imports
+  //   'import/resolver': {
+  //     typescript: {
+  //       paths: ['src'],
+  //       extensions: ['.js', '.jsx', '.ts', '.tsx']
+  //     },
+  //     node: {
+  //       paths: ['src'],
+  //       extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  //     },
+  //   },
+  // },
+  // rules: {
+  //   // Add your own rules here to override ones from the extended configs.
+  //   'react/react-in-jsx-scope': 'off', // Disable the rule that enforces React to be in scope
+
+  // },
+  // ignorePatterns: ['vite.config.ts'],
+  // parserOptions: {
+  //   ecmaFeatures: {
+  //     jsx: true,
+  //   },
+  //   project: ['./tsconfig.json'], // Specify it only for TypeScript files
+  // },
+  // overrides: [
+  //   {
+  //     files: ['*.ts', '*.tsx'],
+  //     rules: {
+  //       // Add your own TypeScript-specific rules here.
+  //     },
+  //     parserOptions: {
+  //       project: ['./tsconfig.json'], // Specify it only for TypeScript files
+  //     },
+  //   },
+  // ],
+};
