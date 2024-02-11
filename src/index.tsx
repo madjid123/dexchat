8a71341d19f43db846ddc072c27fe15620c367e7
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-11 16:28:10 +0100

    Remove unused import statement

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store, persistor } from "./app/store";
