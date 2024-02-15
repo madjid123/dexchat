3f6f8752e4a412681e92455a51d34f520d4b5a7b
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-15 21:11:56 +0100

    Update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
-import svgr from "vite-plugin-svgr";
-import path from "path";
-import { fileURLToPath } from "url";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // because __dirname was showing undefined
 const __filename = fileURLToPath(import.meta.url);
@@ -13,20 +13,23 @@ export default defineConfig({
   server: {
     port: Number.parseInt(process.env.PORT!) || 3000,
   },
+  preview: {
+    port: Number.parseInt(process.env.PORT!) || 3000,
+  },
   plugins: [
     react({
-      include: "**/*.tsx",
+      include: '**/*.tsx',
     }),
     viteTsconfigPaths(),
     svgr({
-      include: "**/*.svg?react",
+      include: '**/*.svg?react',
     }),
   ],
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "~": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+      '~': path.resolve(__dirname, './src'),
     },
   },
 });
