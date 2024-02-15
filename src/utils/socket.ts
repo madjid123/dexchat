e195f56a801a3eaea1cface9bcf20980aeb85cab
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-15 21:17:38 +0100

    Update socket configuration to include token in query

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,8 +1,12 @@
 import io from 'socket.io-client';
-import API_URL from '~/URL';
+const API_URL = import.meta.env.VITE_API_URL;
 const socket = io(API_URL, {
   transports: ['websocket'],
   upgrade: false,
+  autoConnect : true,
+  query : {
+    token : localStorage.getItem('token') || ""
+  }
 });
 socket.connect();
 export default socket;
