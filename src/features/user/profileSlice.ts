b40d29be5bfc9eb53ad6a83b4420a9a5fb37ef05
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-15 21:15:16 +0100

    Refactor profileSlice.ts to use updated dependencies and fix API URL

--- src/features/user/profileSlice.ts
+++ src/features/user/profileSlice.ts
@@ -1,103 +1,125 @@
-import { createSlice, createAsyncThunk, current, Reducer } from '@reduxjs/toolkit';
-import axios, { AxiosError } from 'axios';
+import {
+  createSlice,
+  createAsyncThunk,
+  current,
+  Reducer,
+} from '@reduxjs/toolkit';
+import axios from "~/config/axios"
+import { AxiosError } from 'axios';
 import { ProfileInfo, UserInfo } from './profileSlice.types';
-import API_URL from '~/URL';
-import { RootState } from '~/app/store';
+const API_URL = import.meta.env.VITE_API_URL;
+import { RootState, store } from '~/app/store';
 import { useSelector } from 'react-redux';
+import { CheckisAuth } from './authSlice';
 // Async thunk for updating user info
 export const updateUserInfo = createAsyncThunk(
-    'profile/updateUserInfo',
-    async (userInfo: UserInfo, thunkAPI) => {
-        try {
-            // Your update logic here
-            // For example, make an API call to update user info
-            const cleanUserInfo : Omit<UserInfo,"_id"> = userInfo ;
-            const response = await axios({
-                method: 'post',
-                url: `${API_URL}/user/${userInfo._id}/modify/profile`,
-                withCredentials: true,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                data: cleanUserInfo
-            });
-            return response.data;
-        } catch (e) {
-            const error = e as AxiosError;
-            return thunkAPI.rejectWithValue(error.response?.data);
-        }
+  'profile/updateUserInfo',
+  async (userInfo: UserInfo, thunkAPI) => {
+    try {
+      // Your update logic here
+      // For example, make an API call to update user info
+      const cleanUserInfo: Omit<UserInfo, '_id'> = userInfo;
+      const response = await axios({
+        method: 'post',
+        url: `${API_URL}/user/${userInfo._id}/modify/profile`,
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: cleanUserInfo,
+      });
+      if(response.status === 200){
+
+        thunkAPI.dispatch(CheckisAuth())
+      }
+      return response.data;
+    } catch (e) {
+      const error = e as AxiosError;
+      return thunkAPI.rejectWithValue(error.response?.data.errors);
     }
+  },
 );
 
 // Async thunk for updating profile avatar
 export const updateProfileAvatar = createAsyncThunk(
-    'profile/updateProfileAvatar',
-    async ({ avatarFormData, userId }: { avatarFormData: FormData, userId: string }, thunkAPI) => {
-        try {
-            // Your update logic here
-            // For example, make an API call to upload and update profile avatar
-            const response = await axios({
-                method: 'post',
-                url: `${API_URL}/user/${userId}/modify/avatar`,
-                withCredentials: true,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                data: avatarFormData
-            });
-            console.log(response.data);
-            return response.data;
-        } catch (e) {
-            const error = e as AxiosError;
-            return thunkAPI.rejectWithValue(error.response?.data);
-        }
+  'profile/updateProfileAvatar',
+  async (
+    { avatarFormData, userId }: { avatarFormData: FormData; userId: string },
+    thunkAPI,
+  ) => {
+    try {
+      // Your update logic here
+      // For example, make an API call to upload and update profile avatar
+      const response = await axios({
+        method: 'post',
+        url: `${API_URL}/user/${userId}/modify/avatar`,
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            'Bearer ' + (thunkAPI.getState() as RootState).AuthReducer.token,
+        },
+        data: avatarFormData,
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (e) {
+      const error = e as AxiosError;
+      return thunkAPI.rejectWithValue(error.response?.data.errors);
     }
+  },
 );
 
 // Profile slice
 const profileSlice = createSlice({
-    name: 'profile',
-    initialState: {
-        userInfo: null,
-        avatarFormData: null,
-        loading: false,
-        error: null,
-    } as ProfileInfo,
-    reducers: {
-        setUserInfo: (state, action) => {
-            state.userInfo = action.payload;
-        },
-        setAvatarFormData: (state, action) => {
-            state.avatarFormData = action.payload;
-        }
+  name: 'profile',
+  initialState: {
+    userInfo: {
+      _id: '',
+      email: '',
+      username: '',
+      oldpassword: '',
+      newPassword: '',
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(updateUserInfo.pending, (state) => {
-                state.loading = true;
-                state.error = null;
-            })
-            .addCase(updateUserInfo.fulfilled, (state, action) => {
-                state.loading = false;
-                state.userInfo = action.payload;
-            })
-            .addCase(updateUserInfo.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.payload as string;
-            })
-            .addCase(updateProfileAvatar.pending, (state) => {
-                state.loading = true;
-                state.error = null;
-            })
-            .addCase(updateProfileAvatar.fulfilled, (state, action) => {
-                state.loading = false;
-                state.avatarFormData = action.payload;
-            })
-            .addCase(updateProfileAvatar.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.payload as string;
-            });
+    avatarFormData: new FormData(),
+    loading: false,
+    error: null,
+  } as ProfileInfo,
+  reducers: {
+    setUserInfo: (state, action) => {
+      state.userInfo = action.payload;
     },
+    setAvatarFormData: (state, action) => {
+      state.avatarFormData = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(updateUserInfo.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateUserInfo.fulfilled, (state, action) => {
+        state.loading = false;
+        state.userInfo = action.payload;
+      })
+      .addCase(updateUserInfo.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string[];
+      })
+      .addCase(updateProfileAvatar.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateProfileAvatar.fulfilled, (state, action) => {
+        state.loading = false;
+        state.avatarFormData = action.payload;
+      })
+      .addCase(updateProfileAvatar.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string[];
+      });
+  },
 });
 const { actions } = profileSlice;
 export const { setUserInfo, setAvatarFormData } = actions;
